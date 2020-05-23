42f2ea38f7c1310857f48c961e0c90d9cdc09668
Stefan Janssen <contact@stefanjanssen.nl>
2020-05-23 23:31:00 +0200

    Implement parameterparse tests for strings and unknown types

---
+++ CommandBotFramework.Test/Helpers/ParameterParser/ParseStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CommandBotFramework.Enums;
+using Xunit;
+
+namespace CommandBotFramework.Test.Helpers.ParameterParser
+{
+    public class ParseStrings
+    {
+        [Fact]
+        public void RunTestString()
+        {
+            var input = "09pTF*()^OKL:@K$(@)*%#V<VLVJOMIUMR()I#)I%OI$#PO#";
+            var success = CommandBotFramework.Helpers.ParameterParser.TryParseParameter(ParameterType.String, input, out var output);
+            Assert.True(success);
+            Assert.IsType<string>(output);
+            Assert.Equal(input, output);
+        }
+    }
+}
---
+++ CommandBotFramework.Test/Helpers/ParameterParser/ParseUnknown.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CommandBotFramework.Enums;
+using Xunit;
+
+namespace CommandBotFramework.Test.Helpers.ParameterParser
+{
+    public class ParseUnknown
+    {
+        [Fact]
+        public void RunTestString()
+        {
+            var input = "09pTF*()^OKL:@K$(@)*%#V<VLVJOMIUMR()I#)I%OI$#PO#";
+            var success = CommandBotFramework.Helpers.ParameterParser.TryParseParameter(ParameterType.String, input, out var output);
+            Assert.True(success);
+            Assert.IsType<string>(output);
+            Assert.Equal(input, output);
+        }
+
+        [Fact]
+        public void RunTestNumber()
+        {
+            var input = "5.4";
+            var success = CommandBotFramework.Helpers.ParameterParser.TryParseParameter(ParameterType.String, input, out var output);
+            Assert.True(success);
+            Assert.IsType<string>(output);
+            Assert.Equal(input, output);
+        }
+    }
+}
